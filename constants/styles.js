7029bd1e2531c4d6793bcb18e706d3dedd586b22
alonSadan <asadan@redhat.com>
2021-06-18 01:34:46 +0300

    linter changes. sorry for that

--- constants/styles.js
+++ constants/styles.js
@@ -1,61 +1,59 @@
-import { StyleSheet } from 'react-native'
-import Colors from './colors'
+import { StyleSheet } from "react-native";
+import Colors from "./colors";
 
 export default {
-    screen: StyleSheet.create({
-        wrapper: {
-            flex: 1,
-            flexDirection: 'column',
-            backgroundColor: Colors.SECONDARY,
-        },
-        header: {
-            
-        },
-        body: {
-            flex: 1,
-            padding: 5,
-        }
-    }),
-    mainTitle: {
-        fontFamily: 'Heebo-SemiBold',
-        fontStyle: 'normal',
-        fontSize: 32,
-        lineHeight: 47,
-        letterSpacing: -0.3,
+  screen: StyleSheet.create({
+    wrapper: {
+      flex: 1,
+      flexDirection: "column",
+      backgroundColor: Colors.SECONDARY,
     },
-    secondaryTitle: {
-        fontFamily: 'Heebo-SemiBold',
-        fontStyle: 'normal',
-        fontSize: 21,
-        lineHeight: 35,
-        letterSpacing: -0.3,
+    header: {},
+    body: {
+      flex: 1,
+      padding: 5,
     },
-    lightTitle: {
-        fontFamily: 'Heebo-Regular',
-        fontStyle: 'normal',
-        fontSize: 21,
-        lineHeight: 35,
-        letterSpacing: -0.3,
-    },
-    metadata: {
-        fontFamily: 'Heebo-Medium',
-        fontStyle: 'normal',
-        fontSize: 15,
-        lineHeight: 22,
-        letterSpacing: -0.3,
-    },
-    content: {
-        fontFamily: 'Heebo-Regular',
-        fontStyle: 'normal',
-        fontSize: 17,
-        lineHeight: 25,
-        letterSpacing: -0.3,
-    },
-    info: {
-        fontFamily: 'Heebo-Regular',
-        fontStyle: 'normal',
-        fontSize: 14,
-        lineHeight: 25,
-        letterSpacing: -0.3,
-    }
-}
+  }),
+  mainTitle: {
+    fontFamily: "Heebo-SemiBold",
+    fontStyle: "normal",
+    fontSize: 32,
+    lineHeight: 47,
+    letterSpacing: -0.3,
+  },
+  secondaryTitle: {
+    fontFamily: "Heebo-SemiBold",
+    fontStyle: "normal",
+    fontSize: 21,
+    lineHeight: 35,
+    letterSpacing: -0.3,
+  },
+  lightTitle: {
+    fontFamily: "Heebo-Regular",
+    fontStyle: "normal",
+    fontSize: 21,
+    lineHeight: 35,
+    letterSpacing: -0.3,
+  },
+  metadata: {
+    fontFamily: "Heebo-Medium",
+    fontStyle: "normal",
+    fontSize: 15,
+    lineHeight: 22,
+    letterSpacing: -0.3,
+  },
+  content: {
+    fontFamily: "Heebo-Regular",
+    fontStyle: "normal",
+    fontSize: 17,
+    lineHeight: 25,
+    letterSpacing: -0.3,
+  },
+  info: {
+    fontFamily: "Heebo-Regular",
+    fontStyle: "normal",
+    fontSize: 14,
+    lineHeight: 25,
+    letterSpacing: -0.3,
+  },
+};
--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -1,25 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Switch, TouchableOpacity, Platform, I18nManager } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  Switch,
+  TouchableOpacity,
+  Platform,
+  I18nManager,
+} from "react-native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import * as Notifications from 'expo-notifications'
-import _ from 'lodash'
+import * as Notifications from "expo-notifications";
+import _ from "lodash";
 
-import Styles from './styles'
-import Dictionary from '../../constants/dictionary'
-import Colors from '../../constants/colors'
+import Styles from "./styles";
+import Dictionary from "../../constants/dictionary";
+import Colors from "../../constants/colors";
 
-import * as DB from '../../DB'
-import * as Screen from '../Screen'
-import Card from '../../components/Card/Card'
-import Loading from '../../components/Loading/Loading'
-import Popup from '../../components/Popup/Popup'
+import * as DB from "../../DB";
+import * as Screen from "../Screen";
+import Card from "../../components/Card/Card";
+import Loading from "../../components/Loading/Loading";
+import Popup from "../../components/Popup/Popup";
 
-try { 
-  I18nManager.forceRTL(false)
-  I18nManager.allowRTL(false)
-}
-catch (e) {
-  console.log(e)
+try {
+  I18nManager.forceRTL(false);
+  I18nManager.allowRTL(false);
+} catch (e) {
+  console.log(e);
 }
 
 Notifications.setNotificationHandler({
@@ -30,128 +36,132 @@ Notifications.setNotificationHandler({
   }),
 });
 
-async function registerForPushNotification(){
+async function registerForPushNotification() {
   const isPermGranted = await Notifications.getPermissionsAsync();
-  let finalStatus = isPermGranted.status
-  if (isPermGranted.status != 'granted') {
+  let finalStatus = isPermGranted.status;
+  if (isPermGranted.status != "granted") {
     const { IsPermApproved } = await Notifications.requestPermissionsAsync();
     finalStatus = IsPermApproved.status;
   }
-  if (finalStatus !== 'granted') {
-    alert('Failed to get push token for push notification!');
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token for push notification!");
     return;
   }
   let tokenResponse = await Notifications.getExpoPushTokenAsync();
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
+      lightColor: "#FF231F7C",
     });
   }
-  return tokenResponse.data
+  return tokenResponse.data;
 }
 
 const HomeScreen = ({ navigation, route }) => {
-  const [randomItem, setRandomItem] = useState(undefined)
-  const [dailyPhraseEnabled, setDailyPhraseEnabled] = useState(DB.getDailyPhraseSettings().enabled)
+  const [randomItem, setRandomItem] = useState(undefined);
+  const [dailyPhraseEnabled, setDailyPhraseEnabled] = useState(
+    DB.getDailyPhraseSettings().enabled
+  );
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [notificationTime, setNotificationTime] = useState(new Date())
-  const [popupContent, setPopupContent] = useState(null)
-  const [notificationTrigger, setNotificationTrigger] = useState(false)
+  const [notificationTime, setNotificationTime] = useState(new Date());
+  const [popupContent, setPopupContent] = useState(null);
+  const [notificationTrigger, setNotificationTrigger] = useState(false);
 
   const randomItemRefresh = () => {
-    setRandomItem(undefined)
-    DB.getRandomItem().then(item => setRandomItem(item))
-  }
+    setRandomItem(undefined);
+    DB.getRandomItem().then((item) => setRandomItem(item));
+  };
   const padTimeString = (time) => {
-    const pad = "00" + time
-    return pad.substring(pad.length-2, pad.length)
-  }
+    const pad = "00" + time;
+    return pad.substring(pad.length - 2, pad.length);
+  };
   const getDayGreeting = () => {
-    const now  = new Date()
-    const hour = now.getHours()
+    const now = new Date();
+    const hour = now.getHours();
     if (5 <= hour && hour < 12) {
-      return Dictionary.HOME_SCREEN.GREETINGS.MORNING
+      return Dictionary.HOME_SCREEN.GREETINGS.MORNING;
     }
     if (12 <= hour && hour < 18) {
-      return Dictionary.HOME_SCREEN.GREETINGS.NOON
+      return Dictionary.HOME_SCREEN.GREETINGS.NOON;
     }
     if (18 <= hour && hour < 21) {
-      return Dictionary.HOME_SCREEN.GREETINGS.EVENING
+      return Dictionary.HOME_SCREEN.GREETINGS.EVENING;
     }
     if ((21 <= hour && hour <= 24) || (0 <= hour && hour < 5)) {
-      return Dictionary.HOME_SCREEN.GREETINGS.NIGHT
-    } 
-  }
+      return Dictionary.HOME_SCREEN.GREETINGS.NIGHT;
+    }
+  };
   const handleDailyPhraseSwitch = async (on, selectedDate) => {
     if (on) {
-      DB.sendAnalytics('daily_phrase_enabled', selectedDate.toString())
-      setDailyPhraseEnabled(true)
-      registerForPushNotification().then(token => {
-        DB.updateUserExpoNotificationsToken(token, selectedDate)
-        console.log(token)
+      DB.sendAnalytics("daily_phrase_enabled", selectedDate.toString());
+      setDailyPhraseEnabled(true);
+      registerForPushNotification().then((token) => {
+        DB.updateUserExpoNotificationsToken(token, selectedDate);
+        console.log(token);
         if (token !== undefined) {
           DB.setDailyPhraseSettings({
             enabled: true,
-            time: selectedDate
+            time: selectedDate,
           }).then(() => {
             setPopupContent({
               content: "תזמון פתגם יומי פעיל",
-              timeStamp: Date()
-            })
-          })
+              timeStamp: Date(),
+            });
+          });
           Notifications.cancelAllScheduledNotificationsAsync().then(() => {
-              Notifications.scheduleNotificationAsync({
+            Notifications.scheduleNotificationAsync({
               content: {
                 title: "פתגם אחד ביום",
-                body: 'הפתגם היומי שלך מחכה לך!',
-                data: { notificationType: 'dailyPhrase' },
+                body: "הפתגם היומי שלך מחכה לך!",
+                data: { notificationType: "dailyPhrase" },
               },
               trigger: {
                 hour: selectedDate.getHours(),
                 minute: selectedDate.getMinutes(),
-                repeats: true
+                repeats: true,
               },
-            }).then(resp => {
-              console.log('notification', resp)
-            })
-          })
+            }).then((resp) => {
+              console.log("notification", resp);
+            });
+          });
         }
-      })
+      });
     } else {
-      DB.sendAnalytics('daily_phrase_disabled')
-      setDailyPhraseEnabled(false)
-      Notifications.cancelAllScheduledNotificationsAsync()
+      DB.sendAnalytics("daily_phrase_disabled");
+      setDailyPhraseEnabled(false);
+      Notifications.cancelAllScheduledNotificationsAsync();
       DB.setDailyPhraseSettings({
-        enabled: false
+        enabled: false,
       }).then(() => {
         setPopupContent({
           content: "תזמון פתגם יומי כבוי",
-          timeStamp: Date()
-        })
-      })
+          timeStamp: Date(),
+        });
+      });
     }
-  }
+  };
 
   useEffect(() => {
-    randomItemRefresh()
-    const dailyPhraseSettings = DB.getDailyPhraseSettings()
-    if (dailyPhraseSettings.enabled){
-      setNotificationTime(new Date(dailyPhraseSettings.time))
+    randomItemRefresh();
+    const dailyPhraseSettings = DB.getDailyPhraseSettings();
+    if (dailyPhraseSettings.enabled) {
+      setNotificationTime(new Date(dailyPhraseSettings.time));
     }
     if (dailyPhraseSettings.shouldSeeDailyPhrase) {
-      setNotificationTrigger(true)
+      setNotificationTrigger(true);
     } else {
-      Notifications.addNotificationResponseReceivedListener(response => {
-        if (response.notification.request.content.data.notificationType === 'dailyPhrase'){
-          setNotificationTrigger(true)
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        if (
+          response.notification.request.content.data.notificationType ===
+          "dailyPhrase"
+        ) {
+          setNotificationTrigger(true);
         }
-      })
+      });
     }
-
-  }, [])
+  }, []);
 
   return (
     <Screen.Screen>
@@ -160,63 +170,86 @@ const HomeScreen = ({ navigation, route }) => {
         <Popup {...popupContent} />
         <View style={Styles.welcome.wrapper}>
           <Text style={Styles.welcome.greeting}>{getDayGreeting()}</Text>
-          <Text style={Styles.welcome.sentence}>{Dictionary.HOME_SCREEN.SENTENCE}</Text>
+          <Text style={Styles.welcome.sentence}>
+            {Dictionary.HOME_SCREEN.SENTENCE}
+          </Text>
         </View>
         <View style={Styles.timer.wrapper}>
           <View style={Styles.timer.leftSide.wrapper}>
-            <View style={Styles.timer.leftSide.headerWrapper} >
-              <Text style={Styles.timer.leftSide.headerText}>{Dictionary.HOME_SCREEN.TIMER.TITLE}</Text>
+            <View style={Styles.timer.leftSide.headerWrapper}>
+              <Text style={Styles.timer.leftSide.headerText}>
+                {Dictionary.HOME_SCREEN.TIMER.TITLE}
+              </Text>
               <Switch
                 style={Styles.timer.leftSide.switch}
                 trackColor={{ false: Colors.GRAY, true: Colors.PRIMARY }}
                 thumbColor={Colors.LIGHT_GRAY}
                 ios_backgroundColor="#3e3e3e"
-                onValueChange={(value) => handleDailyPhraseSwitch(value, notificationTime)}
+                onValueChange={(value) =>
+                  handleDailyPhraseSwitch(value, notificationTime)
+                }
                 value={dailyPhraseEnabled}
               />
             </View>
-            <View style={{ flex: 1, margin: 2}}>
-              <Text adjustsFontSizeToFit numberOfLines={2} style={Styles.timer.leftSide.contentText}>{Dictionary.HOME_SCREEN.TIMER.CONTENT}</Text>
+            <View style={{ flex: 1, margin: 2 }}>
+              <Text
+                adjustsFontSizeToFit
+                numberOfLines={2}
+                style={Styles.timer.leftSide.contentText}
+              >
+                {Dictionary.HOME_SCREEN.TIMER.CONTENT}
+              </Text>
             </View>
           </View>
-          <TouchableOpacity style={Styles.timer.rightSide.wrapper} onPress={() => setDatePickerVisibility(true)}>
-              <Text style={Styles.timer.rightSide.digitsText(dailyPhraseEnabled)}>{padTimeString(notificationTime.getHours())}</Text>
-              <Text style={Styles.timer.rightSide.colon}>:</Text>
-              <Text style={Styles.timer.rightSide.digitsText(dailyPhraseEnabled)}>{padTimeString(notificationTime.getMinutes().toString())}</Text>
+          <TouchableOpacity
+            style={Styles.timer.rightSide.wrapper}
+            onPress={() => setDatePickerVisibility(true)}
+          >
+            <Text style={Styles.timer.rightSide.digitsText(dailyPhraseEnabled)}>
+              {padTimeString(notificationTime.getHours())}
+            </Text>
+            <Text style={Styles.timer.rightSide.colon}>:</Text>
+            <Text style={Styles.timer.rightSide.digitsText(dailyPhraseEnabled)}>
+              {padTimeString(notificationTime.getMinutes().toString())}
+            </Text>
           </TouchableOpacity>
           <DateTimePickerModal
             isVisible={isDatePickerVisible}
             mode="time"
             onConfirm={(selectedDate) => {
-              setNotificationTime(selectedDate)
-              setDatePickerVisibility(false)
-              if (dailyPhraseEnabled){
-                handleDailyPhraseSwitch(true, selectedDate)
+              setNotificationTime(selectedDate);
+              setDatePickerVisibility(false);
+              if (dailyPhraseEnabled) {
+                handleDailyPhraseSwitch(true, selectedDate);
               }
             }}
             onCancel={() => setDatePickerVisibility(false)}
           />
-          
         </View>
         <View style={Styles.random}>
-          { randomItem
-            ? <Card
-                item={randomItem}
-                navigation={navigation}
-                customHeader={notificationTrigger ? Dictionary.HOME_SCREEN.TIMER.TITLE : Dictionary.HOME_SCREEN.RANDOM_ITEM.TITLE}
-                handleRefresh={randomItemRefresh}
-                notificationTrigger={notificationTrigger}
-                onClose={() => {
-                  setNotificationTrigger(false)
-                  notificationTrigger && randomItemRefresh()
-                }}
-              />
-            : <Loading />
-          }
+          {randomItem ? (
+            <Card
+              item={randomItem}
+              navigation={navigation}
+              customHeader={
+                notificationTrigger
+                  ? Dictionary.HOME_SCREEN.TIMER.TITLE
+                  : Dictionary.HOME_SCREEN.RANDOM_ITEM.TITLE
+              }
+              handleRefresh={randomItemRefresh}
+              notificationTrigger={notificationTrigger}
+              onClose={() => {
+                setNotificationTrigger(false);
+                notificationTrigger && randomItemRefresh();
+              }}
+            />
+          ) : (
+            <Loading />
+          )}
         </View>
       </Screen.Body>
     </Screen.Screen>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
